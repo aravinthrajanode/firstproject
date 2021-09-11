2188ce7ebc2ed66d26f9e96a7f77e5305dad35a9
aravinthrajanode <sharearavinth03@gmail.com>
2021-09-11 21:57:26 +0530

    node project

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const hbs = require('hbs')
+const app = express()
+const path = require('path');
+const dir = path.join(__dirname,'../public')
+const dirs = path.join(__dirname,'../templates/views')
+const partialPath = path.join(__dirname,'../templates/partial')
+app.use(express.static(dir))
+app.set('view engine','hbs')
+//app.set('view',dirs)
+hbs.registerPartials(partialPath)
+
+
+
+
+app.get('',(req, res)=>{
+    res.render('index',{
+        title:'Weather App',
+        name:'Aravinthraja'
+    })
+})
+app.get('/help',(req, res)=>{
+    res.send("helooo help")
+})
+
+app.listen(3000, ()=>{
+    console.log('server is up')
+})
